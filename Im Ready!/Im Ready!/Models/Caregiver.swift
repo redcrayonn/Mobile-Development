dbbd5f1afaee42c81db014eacd67dd8124a58059
Wouter <woutervermeij@hotmail.com>
2017-11-13 10:34:41 +0100

    added tabbar and screens

---
+++ Im Ready!/Im Ready!/Models/Caregiver.swift
@@ -0,0 +1,13 @@
+//
+//  Caregiver.swift
+//  Im Ready!
+//
+//  Created by Wouter Vermeij on 13/11/2017.
+//  Copyright © 2017 Wouter Vermeij. All rights reserved.
+//
+
+import Foundation
+
+class Caregiver {
+    
+}
