84201b932080d2810af1ed381a3d873eb5caa507
Wouter <woutervermeij@hotmail.com>
2017-11-13 10:02:35 +0100

    first commit!

---
+++ Im Ready!/Im Ready!Tests/Im_Ready_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Im_Ready_Tests.swift
+//  Im Ready!Tests
+//
+//  Created by Wouter Vermeij on 13/11/2017.
+//  Copyright © 2017 Wouter Vermeij. All rights reserved.
+//
+
+import XCTest
+@testable import Im_Ready_
+
+class Im_Ready_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
