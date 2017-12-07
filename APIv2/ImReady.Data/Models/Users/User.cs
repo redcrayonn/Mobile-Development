01fa190744a79d70a7e11680ab9fbc21fa451900
Peter <peterb200295@gmail.com>
2017-12-07 15:01:37 +0100

    APIV2: User gekoppeld. Installeer Entity.identity in data project via nuget

--- APIv2/ImReady.Data/Models/Users/User.cs
+++ APIv2/ImReady.Data/Models/Users/User.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ImReady.Data.Models.Users
 {
-    public abstract class User : EntityModel
+    public abstract class User : IdentityUser
     {
-        //TODO link to AspNetUser
-        public int UserId { get; set; }
-
         public string Firstname { get; set; }
 
         public string Lastname { get; set; }
-
-        public string Phonenumber { get; set; }
-
-        public string Email { get; set; }
 
         public Role Role { get; set; }
     }
