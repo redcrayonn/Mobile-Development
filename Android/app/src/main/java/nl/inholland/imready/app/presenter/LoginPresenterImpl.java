6da19bc16f9802513c5602a0286ce10f97fed8b2
RAZSIEL <geoffreyarkenbout@gmail.com>
2018-01-09 11:11:11 +0100

    Started refactoring code to use the MVP design pattern

---
+++ Android/app/src/main/java/nl/inholland/imready/app/presenter/LoginPresenterImpl.java
@@ -0,0 +1,95 @@
+package nl.inholland.imready.app.presenter;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import nl.inholland.imready.R;
+import nl.inholland.imready.app.ImReadyApplication;
+import nl.inholland.imready.app.logic.PreferenceConstants;
+import nl.inholland.imready.app.persistence.UserCache;
+import nl.inholland.imready.app.view.view.LoginView;
+import nl.inholland.imready.service.BaseClient;
+import nl.inholland.imready.service.model.ApiKeyResponse;
+import nl.inholland.imready.service.rest.AuthenticationService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class LoginPresenterImpl implements LoginPresenter, Callback<ApiKeyResponse> {
+
+    @NonNull
+    private final LoginView loginView;
+    @NonNull
+    private final AuthenticationService service;
+    private final Context context;
+    @NonNull
+    private final BaseClient apiClient;
+
+    public LoginPresenterImpl(@NonNull LoginView loginView, @NonNull BaseClient apiClient) {
+        this.loginView = loginView;
+        this.service = apiClient.getAuthenticationService();
+        this.context = loginView.getContext();
+        this.apiClient = apiClient;
+    }
+
+    @Override
+    public void validateCredentials(String username, String password) {
+        if (TextUtils.isEmpty(username)) {
+            String usernameError = context.getString(R.string.empty_username);
+            loginView.showMessage(usernameError);
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            String passwordError = context.getString(R.string.empty_username);
+            loginView.showMessage(passwordError);
+            return;
+        }
+
+        loginView.showProgress();
+
+        service.login(username, password, "password").enqueue(this);
+    }
+
+    @Override
+    public String getLastUsedUserName() {
+        SharedPreferences settings = context.getSharedPreferences(PreferenceConstants.FILE, MODE_PRIVATE);
+        return settings.getString(PreferenceConstants.LAST_USED_EMAIL, null);
+    }
+
+    @Override
+    public void onResponse(Call<ApiKeyResponse> call, Response<ApiKeyResponse> response) {
+        ApiKeyResponse keyResponse = response.body();
+        if (response.isSuccessful() && keyResponse != null) {
+            if (keyResponse.getUserType() == null) {
+                onFailure(call, new Throwable("User type is null"));
+            }
+
+            // Save the username for next time the app is opened
+            SharedPreferences settings = context.getSharedPreferences(PreferenceConstants.FILE, MODE_PRIVATE);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString(PreferenceConstants.LAST_USED_EMAIL, loginView.getUsername());
+            editor.putString(PreferenceConstants.USER_NAME, keyResponse.getFirstName());
+            editor.apply();
+
+            // Save the auth token for further requests
+            apiClient.setToken(keyResponse.getAccessToken());
+
+            UserCache cache = ImReadyApplication.getInstance().getCache(keyResponse.getUserType());
+            cache.setUserId(keyResponse.getUserId());
+
+            loginView.navigateToHome(keyResponse.getUserType());
+        } else {
+            onFailure(call, new Throwable(context.getString(R.string.unknown_error)));
+        }
+    }
+
+    @Override
+    public void onFailure(Call<ApiKeyResponse> call, Throwable t) {
+        loginView.showLoginError();
+    }
+}
+
---
+++ Android/app/src/main/java/nl/inholland/imready/app/view/view/View.java
@@ -0,0 +1,9 @@
+package nl.inholland.imready.app.view.view;
+
+
+import android.content.Context;
+
+public interface View {
+    void showMessage(String message);
+    Context getContext();
+}
