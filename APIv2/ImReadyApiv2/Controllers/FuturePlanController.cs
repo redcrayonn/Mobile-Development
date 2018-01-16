ccfca84800f27fc87ff39f08cdbc16eb1512f358
Jeffrey Blokker <jef.blokker@gmail.com>
2018-01-16 19:36:48 +0100

    Added enrolling to buildingBlock for client

--- APIv2/ImReadyApiv2/Controllers/FuturePlanController.cs
+++ APIv2/ImReadyApiv2/Controllers/FuturePlanController.cs
@@ -1,4 +1,7 @@
-﻿using ImReady.Service.Services.Interfaces;
+﻿using ImReady.Data.Models;
+using ImReady.Data.Models.Users;
+using ImReady.Service.Services;
+using ImReady.Service.Services.Interfaces;
 using ImReadyApiv2.Results.FutureplanResult;
 using System.Net;
 using System.Web.Http;
@@ -10,14 +13,25 @@ namespace ImReadyApiv2.Controllers
     public class FuturePlanController : ApiController
     {
         private readonly IFutureplanService _planService;
+		private readonly IClientService _clientService;
+		private readonly IBuildingblockService _buildingBlockService;
+		private readonly IClientBuildingBlockService _clientBuildingBlockService;
 
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="planService"></param>
-        public FuturePlanController(IFutureplanService planService)
-        {
+		/// <summary>
+		/// ctor
+		/// </summary>
+		/// <param name="planService"></param>
+		/// <param name="clientService"></param>
+		/// <param name="clientService"></param>
+		/// <param name="clientService"></param>
+		public FuturePlanController (IFutureplanService planService, 
+			IClientService clientService, 
+			IBuildingblockService buildingBlockService, 
+			IClientBuildingBlockService clientBuildingBlockService) {
             _planService = planService;
+			_clientService = clientService;
+			_buildingBlockService = buildingBlockService;
+			_clientBuildingBlockService = clientBuildingBlockService;
         }
 
         /// <summary>
@@ -41,5 +55,31 @@ namespace ImReadyApiv2.Controllers
                 return new StatusCodeResult(HttpStatusCode.InternalServerError, this);
             }
         }
-    }
+
+		/// <summary>
+		/// Enroll to client to a new building block
+		/// </summary>
+		/// <param name="id">Id of the Client</param>
+		/// <param name="buildingBlockId">Id of the building block</param>
+		/// <response code="204">No content</response>
+		/// <response code="500">InternalServerError</response>
+		[Route("api/client/{id}/futureplan/enroll/{buildingBlockId}")]
+		[ResponseType(typeof(FutureplanResult))]
+		public IHttpActionResult Post (string id, string buildingBlockId) {
+			Client client = _clientService.GetClient(id);
+			Buildingblock buildingBlock = _buildingBlockService.getById(buildingBlockId);
+
+			if (client == null || buildingBlock == null) {
+				return NotFound();
+			}
+
+			bool isSuccess = _clientBuildingBlockService.Enroll(id, buildingBlockId);
+
+			if (!isSuccess) {
+				return BadRequest("Could not enroll the client");
+			}
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+	}
 }
