539e6375b840e7bda9807f7871f105be8e04eeb7
Olaf de Mol <olafdemol@hotmail.com>
2018-01-18 14:14:01 +0100

    Component activities empty handled

---
+++ UWP/ImReady/Helpers/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ImReady.Helpers.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            if (value == null)
+                return Visibility.Visible;
+            else if(value is IList<object>)
+            {
+                if (!(value as IList<object>).Any())
+                    return Visibility.Visible;
+                else
+                    return Visibility.Collapsed;
+            }
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
