c65a12e9b516fe50e53c94557f516cb33cd5570f
Olaf de Mol <olafdemol@hotmail.com>
2018-01-18 12:36:27 +0100

    Hide certain stuff on component expander.

---
+++ UWP/ImReady/Helpers/Converters/ActivityStatusToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using ImReady.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ImReady.Helpers.Converters
+{
+    public class ActivityStatusDoneToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            if (value is int)
+            {
+                ActivityStatus status = (ActivityStatus)value;
+                switch(status)
+                {
+                    case ActivityStatus.DONE:
+                        return Visibility.Collapsed;
+                    case ActivityStatus.PENDING:
+                        return Visibility.Collapsed;
+                    default:
+                        return Visibility.Visible;
+                }
+            }
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
