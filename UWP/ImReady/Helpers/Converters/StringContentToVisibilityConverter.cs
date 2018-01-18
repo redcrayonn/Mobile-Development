c823104c21ed09b20ed9040c9f798bab2b9a0824
Olaf de Mol <olafdemol@hotmail.com>
2018-01-18 13:44:26 +0100

    Activity styling geupdatet

--- UWP/ImReady/Helpers/Converters/StringContentToVisibilityConverter.cs
+++ UWP/ImReady/Helpers/Converters/StringContentToVisibilityConverter.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace ImReady.Helpers.Converters
 {
-    class StringContentToVisibilityConverter
+    public class StringContentToVisibilityConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            if (value is string)
+            {
+                var val = value as string;
+                if (string.IsNullOrWhiteSpace(val))
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                    return Visibility.Visible;
+            }
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
