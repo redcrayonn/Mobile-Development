a0b61b53ef22fef2247305b40e0b59a85fd39b8e
Olaf de Mol <olafdemol@hotmail.com>
2018-01-19 03:22:59 +0100

    points

--- UWP/ImReady/Models/FuturePlan.cs
+++ UWP/ImReady/Models/FuturePlan.cs
@@ -9,6 +9,30 @@ namespace ImReady.Models
 {
     public class FuturePlan
     {
+        public string EarnedPoints {
+            get
+            {
+                int points = 0;
+                foreach(var block in Blocks)
+                {
+                    if (block.Components != null)
+                    {
+                        foreach (var component in block.Components)
+                        {
+                            if (component.Activities != null)
+                            {
+                                foreach (var activity in component.Activities)
+                                {
+                                    if (activity.Status == (int)ActivityStatus.DONE)
+                                        points += activity.Points;
+                                }
+                            }
+                        }
+                    }
+                }
+                return points.ToString();
+            }
+        }
         public BuildingBlock[] Blocks { get; set; }
     }
 }
